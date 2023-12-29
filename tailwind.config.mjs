9758c4de3b36a1e2bdf3639c016e91d7be9bdbd8
Javier Eufracio <65838690+Javieer57@users.noreply.github.com>
2023-12-28 18:01:06 -0600

    add ThemeSwitcher component

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: "class",
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Nunito Sans", "sans-serif"],
+      },
+      colors: {
+        white: {
+          DEFAULT: "#FFFFFF",
+          100: "#FAFAFA",
+        },
+        darkBlue: {
+          500: "#2B3844",
+          600: "#202C36",
+          700: "#111517",
+        },
+        gray: {
+          500: "#979797",
+        },
+      },
+    },
   },
   plugins: [],
 };
